bb3da9e2f0dbe14026901b2c2b11dc60cc156e7b
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-10-18 15:17:08 +0530

    Added name and USN loop input

--- ex02.R
+++ ex02.R
@@ -1,36 +1,41 @@
-# Incomplete
+#Incomplete
+noStud <- readline("Enter number of students: ")
+num_courses <- readline("Enter number of courses: ")
 
-noStud <- 10
-num_courses <- 2
-usn <- c()
 student_records <- list()
 student_names <- c()
+usn <- c()
 course_marks <- matrix(c(0))
 
 for (i in 1:noStud){
   student_names[i] = readline("Enter name: ")
-  course_marks[i] = readline(cat("Enter",num_courses,"marks"))
+  #course_marks[i] = readline(cat("Enter",num_courses,"marks"))
 }
-print(student_names)
 
-for (student_index in 1:num_students) {
+for (i in 1:noStud){
+  usn[i] = readline("Enter USN: ")
+}
+
+for (student_index in 1:noStud) {
   student_name <- student_names[student_index]
   total_marks <- sum(course_marks[student_index, ])
   average_marks <- total_marks / num_courses
+  student_usn <- usn[student_index]
   grade <- ifelse(average_marks >= 90, "A",
                   ifelse(average_marks >= 80,"B",
                          ifelse(average_marks >= 70, "C",
                                 ifelse(average_marks >= 60, "D", "F"))))
-  student_record <- list(name = student_name, marks = course_marks[student_index, ],
+  student_record <- list(name = student_name, usn =  student_usn, marks = course_marks[student_index, ],
                          total = total_marks, average = average_marks, grade = grade)
   student_records <- c(student_records, list(student_record))
 }
 
 cat("\nStudent Grade Report:\n")
-for (student_record in student_records) {
-  cat("\nName:", student_record$name, "\n")
-  cat("Marks:", student_record$marks, "\n")
-  cat("Total Marks:", student_record$total, "\n")
-  cat("Average Marks:", student_record$average, "\n")
-  cat("Grade:", student_record$grade, "\n")
+for (studRecord in student_records) {
+  cat("\nName:", studRecord$name, "\n")
+  cat("Marks:", studRecord$usn, "\n")
+  cat("Marks:", studRecord$marks, "\n")
+  cat("Total Marks:", studRecord$total, "\n")
+  cat("Average Marks:", studRecord$average, "\n")
+  cat("Grade:", studRecord$grade, "\n")
 }

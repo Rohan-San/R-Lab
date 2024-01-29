d819a8d5cf78fe38f04b2ccc2548c0b4c9af0a93
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-01-29 20:59:38 +0530

    Delete 0104nameage.R

--- 0104nameage.R
+++
@@ -1,9 +0,0 @@
-# 4. Take name and age input from user and display values.
-# Also print R version
-
-x <- as.character(readline("Enter name: "))
-y <- as.integer(readline("Enter age: "))
-
-print(paste("My name is", x, "and I am", y, "years old.\n"))
-print(sprintf("My name is %s and I am %d years old.\n", x, y))
-cat(R.version.string, "\n")
